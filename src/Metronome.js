7e9cad446555d9927bcffe0a9552c9d55a6295ed
Liz Aitken <liz.a.aitken@gmail.com>
2019-07-02 06:45:18 -0400

    first commit

---
+++ src/Metronome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './Metronome.css';
+
+function Metronome() {
+  return (
+    <div className="big-div-wrapper">
+    </div>
+  );
+}
+
+export default Metronome;
