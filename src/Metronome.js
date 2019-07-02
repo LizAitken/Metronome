f609f8fbc7afaa21d5d3bd40099251df6b2b589f
Liz Aitken <liz.a.aitken@gmail.com>
2019-07-02 07:01:10 -0400

    Setting it up

--- src/Metronome.js
+++ src/Metronome.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './Metronome.css';
 
-function Metronome() {
-  return (
-    <div className="big-div-wrapper">
-    </div>
-  );
+class Metronome extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      playing: false,
+      count: 0,
+      bpm: 100,
+      beatsPerMeasure: 4
+    };
+  }
+
+  render() {
+    const { bpm, playing } = this.state;
+
+    return (
+      <div className="big-div-wrapper">
+        <div className="bpm-slider">
+          <div>{bpm} BPM</div>
+          <input type="range" min="60" max="240" value={bpm}/>
+        </div>
+        <button>{playing ? 'Stop' : 'Start'}</button>
+      </div>
+    );
+  }
 }
 
 export default Metronome;
